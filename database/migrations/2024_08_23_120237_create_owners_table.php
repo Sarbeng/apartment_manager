a6b9710c00e6ced0f406c9c2f6516d219539dbdc
Kwadwo Sarbeng-Baafi <ksarbengbaafi@gmail.com>
2024-08-23 12:12:58 +0000

    Added owners migration

--- database/migrations/2024_08_23_120237_create_owners_table.php
+++ database/migrations/2024_08_23_120237_create_owners_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('owners', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('apartment_block_id')->constrained();
+            $table->string('phone_number');
+            $table->string('address');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
