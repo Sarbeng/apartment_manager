e0d2c02b1c1b1e8a7c78e46b52f29151a2622a55
Kwadwo Sarbeng-Baafi <ksarbengbaafi@gmail.com>
2024-09-05 13:30:29 +0000

    installed blade icons and created a password toggle

--- resources/views/livewire/forms/password-input.blade.php
+++ resources/views/livewire/forms/password-input.blade.php
@@ -3,6 +3,18 @@
     @if ($label)
         <label class=""> {{$label}} </label>
     @endif
-    <input type="{{$type}}" placeholder="{{$placeholder}}" class="{{'border w-full'.$class}}"/>
+    <div class="flex gap-1">
+        <input type="{{$type}}" placeholder="{{$placeholder}}" class="{{'border w-full'.$class}}"/>
+        {{-- Toggle Password Visibility --}}
+        <button type="button" wire:click="togglePasswordVisibility" class="  inset-y-0 right-0 flex items-center px-2">
+            @if ($isPasswordVisible)
+            {{--Show 'hide' icon --}}
+            hide
+            @else
+            {{--Show 'Show' icon --}}
+            Show
+            @endif
+        </button>
+    </div>
     
 </div>
