cb281a613d39a1d7cbd0ff7abb557b8862033f3e
Kwadwo Sarbeng-Baafi <ksarbengbaafi@gmail.com>
2024-09-06 17:17:01 +0000

    deleted livewire components i am not using

--- resources/views/livewire/forms/button.blade.php
+++
@@ -1,6 +0,0 @@
-<div>
-    {{-- Close your eyes. Count to one. That is how long forever feels. --}}
-    <button type="{{$type}}" class="bg-blue-900 px-4 h-10 w-full rounded text-white">
-        <span>{{ $label }}</span>
-    </button>
-</div>
--- resources/views/livewire/forms/input.blade.php
+++
@@ -1,5 +0,0 @@
-<div class="mb-4">
-    <label for="{{$model}}" class="block text-gray-700 text-sm font-bold mb-2">Email</label>
-    <input type="text" wire:model="{{$model}}" id="{{$model}}" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:ring @error('{{$model}}') border-red-500 @enderror">
-   
-</div>
