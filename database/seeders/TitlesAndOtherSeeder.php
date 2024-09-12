fc9ba2188a5cd227c8844c18d0db7a4ebaca3476
Kwadwo Sarbeng-Baafi <ksarbengbaafi@gmail.com>
2024-09-12 13:54:53 +0000

    Research Field and Titles seeder complete

--- database/seeders/TitlesAndOtherSeeder.php
+++ database/seeders/TitlesAndOtherSeeder.php
@@ -13,7 +13,7 @@ class TitlesAndOtherSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        //inserting titles into title table
         DB::table('title')->insert([
             ['short_name'=>'Mr.', 'long_name' => 'Mister'],
             ['short_name'=>'Mrs.', 'long_name' => 'Mistress'],
@@ -28,7 +28,62 @@ class TitlesAndOtherSeeder extends Seeder
             ['short_name'=>'Hajia.', 'long_name' => 'Hajia'],
             
         ]);
+        // insert data into research field table
+        DB::table('research_field')->insert([
+            ['name'=>'Agricultural Sciences'],
+            ['name'=>'Clinical Trials'],
+            ['name'=>'Engineering and Technology'],
+            ['name'=>'Humanities'],
+            ['name'=>'Medical Sciences'],
+            ['name'=>'Social Sciences'],
+        ]);
+        
+        // insert data into research sub field table
+        DB::table('research_subfield')->insert([
+            ['name'=> 'Agricultural Biotechnology'],
+            ['name'=> 'Agriculture, Forestry and Fishery'],
+            ['name'=> 'Animal and Diary Science'],
+            ['name'=> 'Art'],
+            ['name'=> 'Basic Medicine'],
+            ['name'=> 'Biological Sciences'],
+            ['name'=> 'Chemical Engineering'],
+            ['name'=> 'Chemical Sciences'],
+            ['name'=> 'Civil Engineering'],
+            ['name'=> 'Clinical Medicine'],
+            ['name'=> 'Computer and Information Sciences'],
+            ['name'=> 'Earth and Related Evironmental Sciences'],
+            ['name'=> 'Economics and Business'],
+            ['name'=> 'Educational Sciences'],
+            ['name'=> 'Environmental Biotechnology'],
+            ['name'=> 'Environmental engineering '],
+            ['name'=> 'Health Biotechnology'],
+            ['name'=> 'Health sciences'],
+            ['name'=> 'History and archaeology'],
+            ['name'=> 'Industrial Biotechnology'],
+            ['name'=> 'Languages and literature'],
+            ['name'=> 'Law'],
+            ['name'=> 'Materials engineering'],
+            ['name'=> 'Mathematics'],
+            ['name'=> 'Mechanical engineering'],
+            ['name'=> 'Media and communications'],
+            ['name'=> 'Medical engineering'],
+            ['name'=> 'Nano-technology'],
+            ['name'=> 'Other agricultural sciences'],
+            ['name'=> 'Other engineering and tech'],
+            ['name'=> 'Other humanities'],
+            ['name'=> 'Other medical sciences'],
+            ['name'=> 'Other natural sciences'],
+            ['name'=> 'Other social sciences'],
+            ['name'=> 'Philosophy, ethics and religion'],
+            ['name'=> 'Physical sciences'],
+            ['name'=> 'Political Science'],
+            ['name'=> 'Psychology'],
+            ['name'=> 'Social and economic geography'],
+            ['name'=> 'Sociology'],
+            ['name'=> 'Veterinary sciences'],
+            ['name'=> 'Psychology'],
 
-        DB::table('research_field')->insert([]);
+
+        ]);
     }
 }
